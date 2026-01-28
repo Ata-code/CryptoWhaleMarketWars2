f1b581362673678f51f879a9fe5520b688739c1e
Ata-code <ansariataullah95@gmail.com>
2026-01-28 09:11:50 +0530

    before baseapp

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { sdk } from '@farcaster/miniapp-sdk';
+import { useEffect } from 'react';
 
 createRoot(document.getElementById("root")!).render(<App />);
